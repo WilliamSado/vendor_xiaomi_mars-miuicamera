7f8e76da1d60e32b12d2504ba313f669d72e8b73
WilliamSado <williamsado1004@gmail.com>
2024-07-06 03:48:22 +0800

    miuiocamera: Import system.prop

--- products/board.mk
+++ products/board.mk
@@ -1,1 +1,6 @@
 include vendor/xiaomi/mars-miuicamera/common/BoardConfigVendor.mk
+
+MIUICAMERA_PATH := vendor/xiaomi/mars-miuicamera
+
+# Properties
+TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
--- products/miuicamera.mk
+++ products/miuicamera.mk
@@ -1,5 +1,10 @@
 $(call inherit-product, vendor/xiaomi/mars-miuicamera/common/common-vendor.mk)
 
+MIUICAMERA_PATH := vendor/xiaomi/mars-miuicamera
+
+# Properties
+TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
+
 # Overlays
 PRODUCT_PACKAGES += \
     MiuiCameraOverlay
