55fa4756ea1aa647251cfda7dca9f3ce965f6229
WilliamSado <williamsado1004@gmail.com>
2024-07-06 03:53:01 +0800

    miuicamera: Initial permission configs

--- products/miuicamera.mk
+++ products/miuicamera.mk
@@ -1,10 +1,13 @@
 $(call inherit-product, vendor/xiaomi/mars-miuicamera/common/common-vendor.mk)
 
-MIUICAMERA_PATH := vendor/xiaomi/mars-miuicamera
-
-# Properties
-TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
-
 # Overlays
 PRODUCT_PACKAGES += \
     MiuiCameraOverlay
+
+# Priv-app permission
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/mars-miuicamera/configs/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/mars-miuicamera/configs/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
